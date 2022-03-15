b84ef8e8b326d1ed39a11fbdd845a949f99d7785
eseiner <86435840+eseiner@users.noreply.github.com>
2022-03-14 23:30:55 -0400

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,11 @@
+INSERT INTO player
+VALUES
+  (999999996, 'QB', 'Active', 10);
+  
+INSERT INTO player_info
+VALUES
+  (999999996, 'Eli', 'NYC', '04/19/02', 'Caltech', 99, 199);
+
+INSERT INTO passing
+VALUES
+  (999999996, 2020, 'Caltech', 100, 10, 10, 500, 0, 0, 0, 0, 153.8);
