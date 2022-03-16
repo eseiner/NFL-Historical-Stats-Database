398f4670fa4f0122b0859e3809b40181cd8df8d7
eseiner <86435840+eseiner@users.noreply.github.com>
2022-03-16 13:34:50 -0700

    Update load-data.sql

--- load-data.sql
+++ load-data.sql
@@ -3,16 +3,16 @@
 
 SET GLOBAL local_infile = 1; 
 
-LOAD DATA LOCAL INFILE '/Users/halleblend/Desktop/Caltech/sophmore/term2/cs121/finalproject/csvfiles/defense.csv' INTO TABLE defense
-FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE '/Users/halleblend/Desktop/Caltech/sophmore/term2/cs121/finalproject/csvfiles/passing.csv' INTO TABLE passing
-FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
-
 LOAD DATA LOCAL INFILE '/Users/halleblend/Desktop/Caltech/sophmore/term2/cs121/finalproject/csvfiles/player.csv' INTO TABLE player
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE '/Users/halleblend/Desktop/Caltech/sophmore/term2/cs121/finalproject/csvfiles/player_info.csv' INTO TABLE player_info
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE '/Users/halleblend/Desktop/Caltech/sophmore/term2/cs121/finalproject/csvfiles/defense.csv' INTO TABLE defense
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE '/Users/halleblend/Desktop/Caltech/sophmore/term2/cs121/finalproject/csvfiles/passing.csv' INTO TABLE passing
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE '/Users/halleblend/Desktop/Caltech/sophmore/term2/cs121/finalproject/csvfiles/receiving.csv' INTO TABLE receiving
